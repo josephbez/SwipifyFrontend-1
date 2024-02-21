a1ed1029b44cb15c856247970ba68cf62c305b20
Justin R Mealey <justinmealey16@gmail.com>
2024-02-20 17:11:41 -0800

    Added simple functionality for swipe

--- src/Swipescreen.js
+++ src/Swipescreen.js
@@ -1,5 +1,30 @@
 import React from 'react'
 
 export default function Swipescreen(){
-    return <div>Curate your playlist below:</div>
+    return ( 
+    <>
+    <div className='Title'>Curate {playlistObj.name} below:</div>
+    
+    <div>
+    <ul className="delete-list">
+        <li>first</li><li>sec</li>
+    </ul>
+    <ul className='keep-list'>
+        <li>first</li><li>sec</li>
+    </ul>
+    </div>
+
+    <div className='center'>
+        <button>reject song</button>
+        <p>{playlistObj.tracks[0]}</p>
+        <button>keep song</button>
+    </div>
+    </>
+    )
+}
+
+
+let playlistObj = { //will be obtained from backend
+    name: 'myplaylist',
+    tracks: ['t1','t2','t3','t4','t5','t6'],
 }
