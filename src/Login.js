d804a0bcf41fa110fd7c42dc9396210d09e952f9
josephbez <144858176+josephbez@users.noreply.github.com>
2024-03-07 11:46:19 -0800

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
-export default function Login() {
+function Login() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <a className="btn-spotify" href='http://localhost:8000/login'>
-                    Login with Spotify
-                </a>
-            </header>
-        </div>
+        <>
+            <title>Swipify</title>
+                <div class="top-bar">
+                    <div class="small-title">
+                        <h2>Swipify &#9835;</h2>
+                    </div>
+                </div>
+
+                <div class="container">
+                  <div class="center">
+                    <h1 class="big-title">Swipe in to Swipify</h1>
+                    <button class="login-button" href = 'http://localhost:8000/login'>Login with Spotify</button>
+                  </div>
+                </div>
+        </>
     );
 }
 
+export default Login;
